90c318ab774878fd150344c283f37946cb0aa3fa
YitianSusan <bjno8linyitian@gmail.com>
2024-11-24 13:02:16 -0800

    gscore is not correct

--- CSE511-Project-Hotspot-Analysis/src/main/scala/cse512/HotcellAnalysis.scala
+++ CSE511-Project-Hotspot-Analysis/src/main/scala/cse512/HotcellAnalysis.scala
@@ -10,40 +10,122 @@ object HotcellAnalysis {
   Logger.getLogger("org.apache").setLevel(Level.WARN)
   Logger.getLogger("akka").setLevel(Level.WARN)
   Logger.getLogger("com").setLevel(Level.WARN)
+  val logger: Logger = Logger.getLogger(this.getClass.getName)
 
-def runHotcellAnalysis(spark: SparkSession, pointPath: String): DataFrame =
-{
-  // Load the original data from a data source
-  var pickupInfo = spark.read.format("com.databricks.spark.csv").option("delimiter",";").option("header","false").load(pointPath);
-  pickupInfo.createOrReplaceTempView("nyctaxitrips")
-  pickupInfo.show()
+  def runHotcellAnalysis(spark: SparkSession, pointPath: String): DataFrame = {
+    // Load the original data from a data source
+    var pickupInfo = spark.read.format("com.databricks.spark.csv").option("delimiter", ";").option("header", "false").load(pointPath);
+    pickupInfo.createOrReplaceTempView("nyctaxitrips")
+    pickupInfo.show()
 
-  // Assign cell coordinates based on pickup points
-  spark.udf.register("CalculateX",(pickupPoint: String)=>((
-    HotcellUtils.CalculateCoordinate(pickupPoint, 0)
-    )))
-  spark.udf.register("CalculateY",(pickupPoint: String)=>((
-    HotcellUtils.CalculateCoordinate(pickupPoint, 1)
-    )))
-  spark.udf.register("CalculateZ",(pickupTime: String)=>((
-    HotcellUtils.CalculateCoordinate(pickupTime, 2)
-    )))
-  pickupInfo = spark.sql("select CalculateX(nyctaxitrips._c5),CalculateY(nyctaxitrips._c5), CalculateZ(nyctaxitrips._c1) from nyctaxitrips")
-  var newCoordinateName = Seq("x", "y", "z")
-  pickupInfo = pickupInfo.toDF(newCoordinateName:_*)
-  pickupInfo.show()
+    // Assign cell coordinates based on pickup points
+    spark.udf.register("CalculateX", (pickupPoint: String) => ((
+      HotcellUtils.CalculateCoordinate(pickupPoint, 0)
+      )))
+    spark.udf.register("CalculateY", (pickupPoint: String) => ((
+      HotcellUtils.CalculateCoordinate(pickupPoint, 1)
+      )))
+    spark.udf.register("CalculateZ", (pickupTime: String) => ((
+      HotcellUtils.CalculateCoordinate(pickupTime, 2)
+      )))
+    pickupInfo = spark.sql("select CalculateX(nyctaxitrips._c5),CalculateY(nyctaxitrips._c5), CalculateZ(nyctaxitrips._c1) from nyctaxitrips")
+    var newCoordinateName = Seq("x", "y", "z")
+    pickupInfo = pickupInfo.toDF(newCoordinateName: _*)
+    pickupInfo.createOrReplaceTempView("pickupInfo")
+    pickupInfo.show()
 
-  // Define the min and max of x, y, z
-  val minX = -74.50/HotcellUtils.coordinateStep
-  val maxX = -73.70/HotcellUtils.coordinateStep
-  val minY = 40.50/HotcellUtils.coordinateStep
-  val maxY = 40.90/HotcellUtils.coordinateStep
-  val minZ = 1
-  val maxZ = 31
-  val numCells = (maxX - minX + 1)*(maxY - minY + 1)*(maxZ - minZ + 1)
+    // Define the min and max of x, y, z
+    val minX = -74.50 / HotcellUtils.coordinateStep
+    val maxX = -73.70 / HotcellUtils.coordinateStep
+    val minY = 40.50 / HotcellUtils.coordinateStep
+    val maxY = 40.90 / HotcellUtils.coordinateStep
+    val minZ = 1
+    val maxZ = 31
+    val numCells = (maxX - minX + 1) * (maxY - minY + 1) * (maxZ - minZ + 1)
 
-  // YOU NEED TO CHANGE THIS PART
+    // Step 1: Count trips per cell
+    val tripsPerCell = spark.sql(
+      s"""
+         |SELECT DISTINCT
+         |  x,
+         |  y,
+         |  z,
+         |  COUNT(*) as trips_cnt
+         |FROM pickupInfo
+         |WHERE x BETWEEN $minX AND $maxX AND y BETWEEN $minY AND $maxY AND z BETWEEN $minZ AND $maxZ
+         |GROUP BY x, y, z
+         |ORDER BY trips_cnt DESC
+         |""".stripMargin
+    )
+    logger.info("tripsPerCell")
+    tripsPerCell.show()
+    tripsPerCell.createOrReplaceTempView("tripsPerCell")
 
-  return pickupInfo // YOU NEED TO CHANGE THIS PART
-}
+    // Step 2: Compute global statistics
+    val globalStats = spark.sql(
+      """
+        |SELECT
+        | AVG(trips_cnt) as mean,
+        | STDDEV(trips_cnt) as std
+        |FROM tripsPerCell
+        |""".stripMargin
+    ).collect()(0)
+
+    val mean = globalStats.getDouble(0)
+    val std = globalStats.getDouble(1)
+//    logger.info("mean")
+//    logger.info(mean)
+//    logger.info("std")
+//    logger.info(std)
+
+    // cartesian product of tripsPerCell table with itself to calculate neighbors info
+    // neighbor: x, y, z is +- 1 from itself but not fully equal
+    val neighborData = spark.sql(
+      s"""
+         |SELECT
+         |  tp1.x,
+         |  tp1.y,
+         |  tp1.z,
+         |  SUM(tp2.trips_cnt) AS sum_neighbour_trips,
+         |  COUNT(*) AS num_neighbors
+         |FROM tripsPerCell tp1, tripsPerCell tp2
+         |WHERE
+         |  NOT (tp1.x = tp2.x AND tp1.y = tp2.y AND tp1.z = tp2.z) AND
+         |  ABS(tp1.x - tp2.x) <= 1 AND
+         |  ABS(tp1.y - tp2.y) <= 1 AND
+         |  ABS(tp1.z - tp2.z) <= 1
+         |GROUP BY tp1.x, tp1.y, tp1.z
+         |ORDER BY sum_neighbour_trips DESC
+         |""".stripMargin
+    )
+    logger.info("neighborData")
+    neighborData.show()
+    neighborData.createOrReplaceTempView("neighborData")
+
+    // Step 3: Calculate G* statistic for each cell
+    // Register calculateG as a UDF
+    spark.udf.register("calculateG", (sumNeighborTrips, cntNeighbor) =>
+      HotcellUtils.calculateG(sumNeighborTrips, cntNeighbor, numCells, mean, std)
+    )
+
+    val gStarResults = spark.sql(
+      s"""
+         |SELECT
+         |  x,
+         |  y,
+         |  z,
+         |  calculateG(sum_neighbour_trips, num_neighbors) as gScore
+         |FROM neighborData
+         |ORDER BY gScore DESC
+         |LIMIT 50
+         |""".stripMargin
+    )
+
+    gStarResults.createOrReplaceTempView("gStarResults")
+    gStarResults.show()
+    val filteredResults = gStarResults.select("x", "y", "z")
+
+    // Return only x, y, z
+    return filteredResults
+  }
 }
--- CSE511-Project-Hotspot-Analysis/src/main/scala/cse512/HotcellUtils.scala
+++ CSE511-Project-Hotspot-Analysis/src/main/scala/cse512/HotcellUtils.scala
@@ -7,15 +7,13 @@ import java.util.Calendar
 object HotcellUtils {
   val coordinateStep = 0.01
 
-  def CalculateCoordinate(inputString: String, coordinateOffset: Int): Int =
-  {
+  def CalculateCoordinate(inputString: String, coordinateOffset: Int): Int = {
     // Configuration variable:
     // Coordinate step is the size of each cell on x and y
     var result = 0
-    coordinateOffset match
-    {
-      case 0 => result = Math.floor((inputString.split(",")(0).replace("(","").toDouble/coordinateStep)).toInt
-      case 1 => result = Math.floor(inputString.split(",")(1).replace(")","").toDouble/coordinateStep).toInt
+    coordinateOffset match {
+      case 0 => result = Math.floor((inputString.split(",")(0).replace("(", "").toDouble / coordinateStep)).toInt
+      case 1 => result = Math.floor(inputString.split(",")(1).replace(")", "").toDouble / coordinateStep).toInt
       // We only consider the data from 2009 to 2012 inclusively, 4 years in total. Week 0 Day 0 is 2009-01-01
       case 2 => {
         val timestamp = HotcellUtils.timestampParser(inputString)
@@ -25,27 +23,35 @@ object HotcellUtils {
     return result
   }
 
-  def timestampParser (timestampString: String): Timestamp =
-  {
+  def timestampParser(timestampString: String): Timestamp = {
     val dateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss")
     val parsedDate = dateFormat.parse(timestampString)
     val timeStamp = new Timestamp(parsedDate.getTime)
     return timeStamp
   }
 
-  def dayOfYear (timestamp: Timestamp): Int =
-  {
+  def dayOfYear(timestamp: Timestamp): Int = {
     val calendar = Calendar.getInstance
     calendar.setTimeInMillis(timestamp.getTime)
     return calendar.get(Calendar.DAY_OF_YEAR)
   }
 
-  def dayOfMonth (timestamp: Timestamp): Int =
-  {
+  def dayOfMonth(timestamp: Timestamp): Int = {
     val calendar = Calendar.getInstance
     calendar.setTimeInMillis(timestamp.getTime)
     return calendar.get(Calendar.DAY_OF_MONTH)
   }
 
-  // YOU NEED TO CHANGE THIS PART
+  def calculateG(sum_neighbor_trips: Double, num_neighbors: Double, numCells: Double, mean: Double, std: Double): Double = {
+    // cell: one unique (x, y, z) combination
+    // x: # of rides picked up at a cell
+    // n: total number of cells
+    // weight: 1
+    // j..n is all neighbors of cell i
+    // formula reference: https://sigspatial2016.sigspatial.org/giscup2016/problem
+
+    val numerator = sum_neighbor_trips - mean * num_neighbors
+    val denominator = (std * math.sqrt(((num_neighbors * numCells) - (num_neighbors * num_neighbors)) / (numCells - 1.0)))
+    return numerator / denominator
+  }
 }
